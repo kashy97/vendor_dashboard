b84eea9c80e122bb7c1de268b0be906614fa0810
unknown <admin@D-6099.pmplcorp.com>
2022-02-23 17:47:12 +0530

    Changes updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import DashBoard from "./pages/dashboard";
+import Profile from "./pages/profile";
+import NavBar from "./components/Navbar";
+// import Flow from "./components/flow";
+import Team from "./pages/team";
+import IAdd from "./pages/invoices_form";
+import Orders from "./pages/invoices";
+import Vendors from "./pages/vendors";
+import VAdd from "./pages/vendors_add";
+import ROrders from "./pages/release_orders";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      {/* <Flow /> */}
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<DashBoard />} />
+        <Route path="profile" element={<Profile />}>
+          <Route path=":edit" element={<Team />} />
+        </Route>
+        <Route path="vendors" element={<Vendors />}>
+          <Route path=":add" element={<VAdd />} />
+        </Route>
+        <Route path="invoices" element={<Orders />}>
+          <Route path=":add" element={<IAdd />} />
+        </Route>
+        <Route path="/release_orders" element={<ROrders />} />
+      </Routes>
     </div>
   );
 }
